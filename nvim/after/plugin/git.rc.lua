22da553e7f0dfd8284ff66d4a1a4da9ab2baa10f
yossydev <codeyy021@gmail.com>
2023-09-01 21:56:44 +0900

    update git

--- nvim/after/plugin/git.rc.lua
+++ nvim/after/plugin/git.rc.lua
@@ -5,7 +5,15 @@ git.setup({
   keymaps = {
     -- Open blame window
     blame = "gb",
+    -- Close blame window
+    quit_blame = "q",
+    -- Open blame commit
+    blame_commit = "<CR>",
     -- Open file/folder in git repository
     browse = "go",
-  }
+    -- Opens a new diff that compares against the current index
+    diff = "gf",
+    -- Close git diff
+    diff_close = "gD",
+  },
 })

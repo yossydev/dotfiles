b33f59962f26867856a0c87093d8cc4497766b6d
yossydev <codeyy021@gmail.com>
2024-02-23 17:13:45 +0900

    feat: update neovim for biome

--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -109,6 +109,11 @@ nvim_lsp.rust_analyzer.setup {
   capabilities = capabilities
 }
 
+nvim_lsp.biome.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -1,9 +1,9 @@
 local status, null_ls = pcall(require, "null-ls")
-if (not status) then return end
+if not status then return end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-local lsp_formatting = function(bufnr)
+local function lsp_formatting(bufnr)
   vim.lsp.buf.format({
     filter = function(client)
       return client.name == "null-ls"
@@ -12,50 +12,63 @@ local lsp_formatting = function(bufnr)
   })
 end
 
-local file_exists = function(file)
+local function file_exists(file)
   local f = io.open(file, "r")
-  if f ~= nil then io.close(f) return true else return false end
+  if f then
+    f:close()
+    return true
+  end
+  return false
 end
 
-null_ls.setup {
-  sources = {
+-- 共通のon_attach関数
+local function on_attach(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function() lsp_formatting(bufnr) end,
+    })
+  end
+end
+
+local function setup_null_ls(sources)
+  null_ls.setup({
+    sources = sources,
+    on_attach = on_attach,
+  })
+end
+
+local rootPath = vim.fn.getcwd()
+local biomeJsonPath = rootPath .. "/biome.json"
+
+if file_exists(biomeJsonPath) then
+  setup_null_ls({
+    null_ls.builtins.formatting.rome.with({ command = "biome" }),
+    null_ls.builtins.diagnostics.fish,
+  })
+  print("biome.json found. LSP formatting with Biome is enabled.")
+else
+  setup_null_ls({
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.diagnostics.eslint.with({
-     extra_args = function(params)
+      extra_args = function(params)
         local file_types = {"eslint.config.js", "eslint.config.mjs", ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.yaml", ".eslintrc.yml", ".eslintrc.json", ".eslintrc"}
-        for _, file_type in pairs(file_types) do
-           local config_path = params.root .. '/' .. file_type
-            if file_exists(config_path) then
-              return {
-                "--config",
-                config_path
-              }
-            end
+        for _, file_type in ipairs(file_types) do
+          local config_path = params.root .. '/' .. file_type
+          if file_exists(config_path) then
+            return { "--config", config_path }
+          end
         end
         return {}
       end,
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
+      diagnostics_format = '[eslint] #{m}\n(#{c})',
     }),
-    null_ls.builtins.diagnostics.fish
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          lsp_formatting(bufnr)
-        end,
-      })
-    end
-  end
-}
+    null_ls.builtins.diagnostics.fish,
+  })
+end
 
-vim.api.nvim_create_user_command(
-  'DisableLspFormatting',
-  function()
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
-  end,
-  { nargs = 0 }
-)
+vim.api.nvim_create_user_command('DisableLspFormatting', function()
+  vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+end, { nargs = 0 })

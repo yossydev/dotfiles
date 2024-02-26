6d35a510d681f5c1ea0630e868fd9509640effdf
yossydev <codeyy021@gmail.com>
2024-02-26 14:12:05 +0900

    update nvim

--- nvim/plugin/0_mason.rc.lua
+++ nvim/plugin/0_mason.rc.lua
@@ -8,5 +8,6 @@ mason.setup({
 })
 
 lspconfig.setup {
-  automatic_installation = true
+  automatic_installation = true,
+  ensure_installed = { "sumneko_lua", "tailwindcss" },
 }
--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -1,74 +1,89 @@
 local status, null_ls = pcall(require, "null-ls")
-if not status then return end
+if not status then
+	return
+end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local function lsp_formatting(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      return client.name == "null-ls"
-    end,
-    bufnr = bufnr,
-  })
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 local function file_exists(file)
-  local f = io.open(file, "r")
-  if f then
-    f:close()
-    return true
-  end
-  return false
+	local f = io.open(file, "r")
+	if f then
+		f:close()
+		return true
+	end
+	return false
 end
 
 -- 共通のon_attach関数
 local function on_attach(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function() lsp_formatting(bufnr) end,
-    })
-  end
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
 end
 
 local function setup_null_ls(sources)
-  null_ls.setup({
-    sources = sources,
-    on_attach = on_attach,
-  })
+	null_ls.setup({
+		sources = sources,
+		on_attach = on_attach,
+	})
 end
 
 local rootPath = vim.fn.getcwd()
 local biomeJsonPath = rootPath .. "/biome.json"
 
 if file_exists(biomeJsonPath) then
-  setup_null_ls({
-    null_ls.builtins.formatting.rome.with({ command = "biome" }),
-    null_ls.builtins.diagnostics.fish,
-  })
-  print("biome.json found. LSP formatting with Biome is enabled.")
+	setup_null_ls({
+		null_ls.builtins.formatting.rome.with({ command = "biome" }),
+		null_ls.builtins.diagnostics.fish,
+	})
+	print("biome.json found. LSP formatting with Biome is enabled.")
 else
-  setup_null_ls({
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.eslint.with({
-      extra_args = function(params)
-        local file_types = {"eslint.config.js", "eslint.config.mjs", ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.yaml", ".eslintrc.yml", ".eslintrc.json", ".eslintrc"}
-        for _, file_type in ipairs(file_types) do
-          local config_path = params.root .. '/' .. file_type
-          if file_exists(config_path) then
-            return { "--config", config_path }
-          end
-        end
-        return {}
-      end,
-      diagnostics_format = '[eslint] #{m}\n(#{c})',
-    }),
-    null_ls.builtins.diagnostics.fish,
-  })
+	setup_null_ls({
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.diagnostics.eslint.with({
+			extra_args = function(params)
+				local file_types = {
+					"eslint.config.js",
+					"eslint.config.mjs",
+					".eslintrc.js",
+					".eslintrc.cjs",
+					".eslintrc.yaml",
+					".eslintrc.yml",
+					".eslintrc.json",
+					".eslintrc",
+				}
+				for _, file_type in ipairs(file_types) do
+					local config_path = params.root .. "/" .. file_type
+					if file_exists(config_path) then
+						return { "--config", config_path }
+					end
+				end
+				return {}
+			end,
+			diagnostics_format = "[eslint] #{m}\n(#{c})",
+		}),
+		null_ls.builtins.diagnostics.fish,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.clang_format,
+	})
 end
 
-vim.api.nvim_create_user_command('DisableLspFormatting', function()
-  vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+vim.api.nvim_create_user_command("DisableLspFormatting", function()
+	vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
 end, { nargs = 0 })

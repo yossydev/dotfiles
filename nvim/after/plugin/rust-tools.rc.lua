319fe38baefb219de193e4bedca4fc252f52c6f2
yossydev <codeyy021@gmail.com>
2024-03-25 01:41:09 +0900

    feat: packer → lazy

--- nvim/after/plugin/rust-tools.rc.lua
+++ nvim/after/plugin/rust-tools.rc.lua
@@ -1,41 +1,40 @@
 local rt = require("rust-tools")
 
 local function on_attach(client, buffer)
-  -- This callback is called when the LSP is atttached/enabled for this buffer
-  -- we could set keymaps related to LSP, etc here.
+	-- This callback is called when the LSP is atttached/enabled for this buffer
+	-- we could set keymaps related to LSP, etc here.
 end
 
-
 local opts = {
-  tools = {
-    runnables = {
-      use_telescope = true,
-    },
-    inlay_hints = {
-      auto = true,
-      show_parameter_hints = false,
-      parameter_hints_prefix = "",
-      other_hints_prefix = "",
-    },
-  },
+	tools = {
+		runnables = {
+			use_telescope = true,
+		},
+		inlay_hints = {
+			auto = true,
+			show_parameter_hints = false,
+			parameter_hints_prefix = "",
+			other_hints_prefix = "",
+		},
+	},
 
-  -- all the opts to send to nvim-lspconfig
-  -- these override the defaults set by rust-tools.nvim
-  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-  server = {
-    -- on_attach is a callback called when the language server attachs to the buffer
-    on_attach = on_attach,
-    settings = {
-      -- to enable rust-analyzer settings visit:
-      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-      ["rust-analyzer"] = {
-        -- enable clippy on save
-        checkOnSave = {
-          command = "clippy",
-        },
-      },
-    },
-  },
+	-- all the opts to send to nvim-lspconfig
+	-- these override the defaults set by rust-tools.nvim
+	-- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+	server = {
+		-- on_attach is a callback called when the language server attachs to the buffer
+		on_attach = on_attach,
+		settings = {
+			-- to enable rust-analyzer settings visit:
+			-- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+			["rust-analyzer"] = {
+				-- enable clippy on save
+				checkOnSave = {
+					command = "clippy",
+				},
+			},
+		},
+	},
 }
 
 rt.setup(opts)
--- nvim/init.lua
+++ nvim/init.lua
@@ -4,19 +4,19 @@ require("maps")
 require("plugins")
 
 local has = function(x)
-  return vim.fn.has(x) == 1
+	return vim.fn.has(x) == 1
 end
 
-local is_mac = has "macunix"
-local is_win = has "win32"
+local is_mac = has("macunix")
+local is_win = has("win32")
 
-vim.api.nvim_set_var('loaded_netrw', 1)
-vim.api.nvim_set_var('loaded_netrwPlugin', 1)
+vim.api.nvim_set_var("loaded_netrw", 1)
+vim.api.nvim_set_var("loaded_netrwPlugin", 1)
 vim.g.rustfmt_autosave = 1
 
 if is_mac then
-  require('macos')
+	require("macos")
 end
 if is_win then
-  require('windows')
+	require("windows")
 end
